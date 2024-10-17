612cd60c6d0ca16c4138e511f9ac8d48dcaf9335
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-17 17:27:21 +0700

    deon navbar

--- src/app/components/ts/navbar.component.ts
+++ src/app/components/ts/navbar.component.ts
@@ -12,14 +12,24 @@ import { WidthCheckService } from '../../hooks/width-check.service';
 })
 export class NavbarComponent {
   imgSrc: string = 'assets/svg/logo-no-background.svg';
-  imgSrc2: string = 'assets/svg/open-menu-svgrepo-com.svg'
+  imgSrc2: string = 'assets/svg/menu-svgrepo-com.svg';
+
+  bgClass: string = 'bg-gradient-to-r from-slate-900 to-slate-700'
   aClass: string = 'text-white px-4 py-2 h-full w-full flex justify-center items-center font-bold';
+  aClass2: string = 'w-full flex justify-center items-center text-white'
+
   innerWidth: number = 0;
+  isMenuOpen: boolean = false
 
   constructor(private widthCheck: WidthCheckService) {
     this.innerWidth = this.widthCheck.innerWidth;
     window.addEventListener('resize', () => {
       this.innerWidth = this.widthCheck.innerWidth;
     });
+  }
+
+  toggleMenu() {
+    this.isMenuOpen =!this.isMenuOpen;
+    console.log(this.isMenuOpen);
   }
 }
