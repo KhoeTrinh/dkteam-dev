504bedac7055781db07ab9e69dbf8abd0c6099ec
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-17 18:22:59 +0700

    done navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductComponent } from './pages/product/product.component';
 import { AboutmeComponent } from './pages/aboutme/aboutme.component';
 import { ContactusComponent } from './pages/contactus/contactus.component';
+import { SigninComponent } from './pages/auth/signin/signin.component';
+import { SignupComponent } from './pages/auth/signup/signup.component';
 
 export const routes: Routes = [
   {
@@ -20,5 +22,13 @@ export const routes: Routes = [
   {
     path: 'contactus',
     component: ContactusComponent,
+  },
+  {
+    path: 'signin',
+    component: SigninComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
   },
 ];
--- src/app/components/ts/navbar.component.ts
+++ src/app/components/ts/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
-import { WidthCheckService } from '../../hooks/width-check.service';
+import { WidthCheckService } from '../../services/width-check.service';
 
 @Component({
   selector: 'app-navbar',
@@ -15,7 +15,7 @@ export class NavbarComponent {
   imgSrc2: string = 'assets/svg/menu-svgrepo-com.svg';
 
   bgClass: string = 'bg-gradient-to-r from-slate-900 to-slate-700'
-  aClass: string = 'text-white px-4 py-2 h-full w-full flex justify-center items-center font-bold';
+  aClass: string = 'text-white text-sm px-4 py-2 h-full w-full flex justify-center items-center font-bold';
   aClass2: string = 'w-full flex justify-center items-center text-white'
 
   innerWidth: number = 0;
