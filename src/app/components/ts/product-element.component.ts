13815a1ea0f764f861e3bbf25eca83b583e49502
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-19 15:27:02 +0700

    done modify product page

--- src/app/components/ts/product-element.component.ts
+++ src/app/components/ts/product-element.component.ts
@@ -1,14 +1,14 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { DatePipe, NgOptimizedImage } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-element',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, DatePipe],
   templateUrl: '../html/product-element.component.html',
   styleUrl: '../css/product-element.component.css',
 })
-export class ProductElementComponent {
+export class ProductElementComponent implements OnInit {
   tClass: string =
     'bg-gradient-to-t from-sky-300 via-fuchsia-50 to-emerald-50 bg-clip-text text-transparent';
   bgClass: string = 'bg-gradient-to-r from-slate-900 to-slate-700';
@@ -16,11 +16,21 @@ export class ProductElementComponent {
   imgSrc2: string = 'assets/svg/arrow-narrow-right-svgrepo-com-black.svg';
   imgSrc3: string = 'assets/svg/gear-10-svgrepo-com.svg';
   imgSrc4: string = 'assets/svg/file-pencil-alt-svgrepo-com.svg';
-  imgSrc5: string = '../../../assets/svg/user-svgrepo-com.svg';
+  imgSrc5: string = 'assets/svg/user-svgrepo-com.svg';
   commentOpen: boolean = false;
+  authors: any = ''
+  comments: any = ''
+
+  @Input() productData: any = ''
+
+  ngOnInit() {
+    this.authors = this.productData.author
+    this.comments = this.productData.comment
+    console.log(this.comments);
+  }
   commentClick(e: Event) {
     e.preventDefault();
     this.commentOpen = !this.commentOpen;
-    console.log(this.commentOpen);
+    console.log(this.productData);
   }
 }
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -18,8 +18,235 @@ export class ProductComponent {
   imgSrc2: string = 'assets/svg/arrow-narrow-right-svgrepo-com-black.svg';
   imgSrc3: string = 'assets/svg/gear-10-svgrepo-com.svg';
   imgSrc4: string = 'assets/svg/file-pencil-alt-svgrepo-com.svg';
-  imgSrc5: string = '../../../assets/svg/user-svgrepo-com.svg';
+  imgSrc5: string = 'assets/svg/user-svgrepo-com.svg';
   innerWidth: number = 0;
+
+  productArray: Array<any> = [
+    {
+      id: '0c548a0a-4914-42f8-a5d0-887ba5f70f',
+      link: 'https://www.youtube.com/watch?v=T9ABgBIYS1g',
+      title: 'File Sharing',
+      description: 'A product created primarily for file sharing',
+      publishDate: '2024-10-10T01:37:55.866Z',
+      image: this.imgSrc4,
+      author: [
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '1'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '2'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '3'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '4'
+          },
+        },
+      ],
+      comment: [
+        {
+          id: 'f5d9ee62-2174b65-b1f3-0f30f1be',
+          description: 'I love it',
+          author: {
+            username: 'Khoa',
+            userImage: this.imgSrc5,
+          },
+        },
+        {
+          id: 'f5d9ee62-217b65-b1f2840f30f1be',
+          description: 'I love it',
+          author: {
+            username: 'Khoa',
+            userImage: this.imgSrc5,
+          },
+        },
+      ],
+    },
+    {
+      id: '0c548a0a-4914-42-a5d0-887ba5f70f',
+      link: 'https://www.youtube.com/watch?v=T9ABgBIYS1g',
+      title: 'File Sharing 2',
+      description: 'A product created primarily for file sharing',
+      publishDate: '2024-10-10T01:37:55.866Z',
+      image: this.imgSrc4,
+      author: [
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '1'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '2'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '3'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '4'
+          },
+        },
+      ],
+      comment: [
+        {
+          id: 'f5d9ee62-2173-5-b1f3-740f30f1be',
+          description: 'I love it',
+          author: {
+            username: 'Khoa',
+            userImage: this.imgSrc5,
+          },
+        },
+        {
+          id: 'f5d9ee62-2173-4b65-b1f3-7f30f1be',
+          description: 'I love it',
+          author: {
+            username: 'Khoa',
+            userImage: this.imgSrc5,
+          },
+        },
+      ],
+    },
+    {
+      id: '0c548a0a-4914-42f8-a5d0-88ba3a5f70f',
+      link: 'https://www.youtube.com/watch?v=T9ABgBIYS1g',
+      title: 'File Sharing 3',
+      description: 'A product created primarily for file sharing',
+      publishDate: '2024-10-10T01:37:55.866Z',
+      image: this.imgSrc4,
+      author: [
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '1'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '2'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '3'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '4'
+          },
+        },
+      ],
+      comment: [
+        {
+          id: 'f5d9ee62-2173-4b65-b1f40f30f1be',
+          description: 'I love it',
+          author: {
+            username: 'Khoa',
+            userImage: this.imgSrc5,
+          },
+        },
+        {
+          id: 'f5d9ee62-2173-4b65-b1f3-7280f1be',
+          description: 'I love it',
+          author: {
+            username: 'Khoa',
+            userImage: this.imgSrc5,
+          },
+        },
+      ],
+    },
+    {
+      id: '0c548a0a-4914-42f8-a5d0-887b3a5f70f',
+      link: 'https://www.youtube.com/watch?v=T9ABgBIYS1g',
+      title: 'File Sharing 4',
+      description: 'A product created primarily for file sharing',
+      publishDate: '2024-10-10T01:37:55.866Z',
+      image: this.imgSrc4,
+      author: [
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '1'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '2'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '3'
+          },
+        },
+        {
+          authorProd: {
+            userImage: this.imgSrc5,
+            username: 'Khoa',
+            id: '4'
+          },
+        },
+      ],
+      comment: [
+        {
+          id: 'f5d9ee62-2173-4b653-72840f30f1b',
+          description: 'I love it',
+          author: {
+            username: 'Khoa',
+            userImage: this.imgSrc5,
+          },
+        },
+        {
+          id: 'f5d9ee62-2173-4b65-b1f30f30f1be',
+          description: 'I love it',
+          author: {
+            username: 'Khoa',
+            userImage: this.imgSrc5,
+          },
+        },
+      ],
+    },
+  ];
 
   constructor(private widthCheck: WidthCheckService) {
     this.innerWidth = this.widthCheck.innerWidth;
