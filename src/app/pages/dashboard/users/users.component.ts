fce3869ae80572e69ccc2726cf5de25b2fca97f8
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-25 17:23:01 +0700

    nahhhhhhhh

--- src/app/pages/dashboard/users/users.component.ts
+++ src/app/pages/dashboard/users/users.component.ts
@@ -1,12 +1,267 @@
-import { Component } from '@angular/core';
+import { NgFor, NgOptimizedImage } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, NgFor],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
 export class UsersComponent {
+  imgSrc: string = 'assets/svg/arrow-narrow-right-svgrepo-com-black.svg';
+  imgSrc2: string = 'assets/svg/user-svgrepo-com.svg';
+  imgSrc3: string = 'assets/svg/pen-square-svgrepo-com-white.svg';
+  imgSrc4: string = 'assets/png/logo-color.png';
+  imgSrc5: string = 'assets/svg/arrow-prev-small-svgrepo-com.svg';
+  imgSrc6: string = 'assets/svg/arrow-next-small-svgrepo-com.svg';
+  bgClass: string = 'bg-gradient-to-r from-slate-900 to-slate-700';
+  bgClass2: string = 'bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-slate-300 via-rose-50 to-teal-50'
+  author: any;
+  currentIndices: number[] = []
+  constructor() {
+    this.author = this.userArray.map((author) => author.authorProd);
+    this.currentIndices = Array(this.userArray.length).fill(0)
+  }
 
+  moveSlider(i: number, direction: number) {
+    const numberOfProduct = this.author[i].length
+    this.currentIndices[i] = (this.currentIndices[i] + direction + numberOfProduct) % numberOfProduct
+    const translateXValue = `translateX(-${100 * this.currentIndices[i]}%)`
+    const slider = document.getElementById(`slider-${i}`)
+    if (slider) {
+      slider.style.transform = translateXValue;
+    }
+  }
+
+  userArray: Array<any> = [
+    {
+      id: '0c548a0a-4914-42f8-a5d0-887ba5f70f',
+      username: 'khoa',
+      email: 'khoa@example.com',
+      userImage: this.imgSrc2,
+      authorProd: [],
+      aboutme: {
+        title: 'khoa',
+        description: 'khoa is a great',
+        image: this.imgSrc2,
+      },
+    },
+    {
+      id: 2,
+      username: 'khoa',
+      email: 'khoa@example.com',
+      userImage: this.imgSrc2,
+      authorProd: [
+        {
+          author: {
+            id: 3,
+            title: 'khoa product',
+            productImage: this.imgSrc4,
+          },
+        },
+        {
+          author: {
+            id: 4,
+            title: 'khoa product 2',
+            productImage: this.imgSrc4,
+          },
+        },
+      ],
+      aboutme: {
+        title: 'khoa',
+        description: 'khoa is a great',
+        image: this.imgSrc2,
+      },
+    },
+    {
+      id: 3,
+      username: 'khoa',
+      email: 'khoa@example.com',
+      userImage: this.imgSrc2,
+      authorProd: [
+        {
+          author: {
+            id: 5,
+            title: 'khoa product',
+            productImage: this.imgSrc4,
+          },
+        },
+        {
+          author: {
+            id: 6,
+            title: 'khoa product 2',
+            productImage: this.imgSrc4,
+          },
+        },
+      ],
+      aboutme: {
+        title: 'khoa',
+        description: 'khoa is a great',
+        image: this.imgSrc2,
+      },
+    },
+    {
+      id: 4,
+      username: 'khoa',
+      email: 'khoa@example.com',
+      userImage: this.imgSrc2,
+      authorProd: [
+        {
+          author: {
+            id: 7,
+            title: 'khoa product',
+            productImage: this.imgSrc4,
+          },
+        },
+        {
+          author: {
+            id: 8,
+            title: 'khoa product 2',
+            productImage: this.imgSrc4,
+          },
+        },
+      ],
+      aboutme: {
+        title: 'khoa',
+        description: 'khoa is a great',
+        image: this.imgSrc2,
+      },
+    },
+    {
+      id: 5,
+      username: 'khoa',
+      email: 'khoa@example.com',
+      userImage: this.imgSrc2,
+      authorProd: [
+        {
+          author: {
+            id: 9,
+            title: 'khoa product',
+            productImage: this.imgSrc4,
+          },
+        },
+        {
+          author: {
+            id: 10,
+            title: 'khoa product 2',
+            productImage: this.imgSrc4,
+          },
+        },
+      ],
+      aboutme: {
+        title: 'khoa',
+        description: 'khoa is a great',
+        image: this.imgSrc2,
+      },
+    },
+    {
+      id: 6,
+      username: 'khoa',
+      email: 'khoa@example.com',
+      userImage: this.imgSrc2,
+      authorProd: [
+        {
+          author: {
+            id: 11,
+            title: 'khoa product',
+            productImage: this.imgSrc4,
+          },
+        },
+        {
+          author: {
+            id: 12,
+            title: 'khoa product 2',
+            productImage: this.imgSrc4,
+          },
+        },
+      ],
+      aboutme: {
+        title: 'khoa',
+        description: 'khoa is a great',
+        image: this.imgSrc2,
+      },
+    },
+    {
+      id: 7,
+      username: 'khoa',
+      email: 'khoa@example.com',
+      userImage: this.imgSrc2,
+      authorProd: [
+        {
+          author: {
+            id: 13,
+            title: 'khoa product',
+            productImage: this.imgSrc4,
+          },
+        },
+        {
+          author: {
+            id: 14,
+            title: 'khoa product 2',
+            productImage: this.imgSrc4,
+          },
+        },
+      ],
+      aboutme: {
+        title: 'khoa',
+        description: 'khoa is a great',
+        image: this.imgSrc2,
+      },
+    },
+    {
+      id: 8,
+      username: 'khoa',
+      email: 'khoa@example.com',
+      userImage: this.imgSrc2,
+      authorProd: [
+        {
+          author: {
+            id: 15,
+            title: 'khoa product',
+            productImage: this.imgSrc4,
+          },
+        },
+        {
+          author: {
+            id: 16,
+            title: 'khoa product 2',
+            productImage: this.imgSrc4,
+          },
+        },
+      ],
+      aboutme: {
+        title: 'khoa',
+        description: 'khoa is a great',
+        image: this.imgSrc2,
+      },
+    },
+    {
+      id: 9,
+      username: 'khoa',
+      email: 'khoa@example.com',
+      userImage: this.imgSrc2,
+      authorProd: [
+        {
+          author: {
+            id: 17,
+            title: 'khoa product',
+            productImage: this.imgSrc4,
+          },
+        },
+        {
+          author: {
+            id: 18,
+            title: 'khoa product 2',
+            productImage: this.imgSrc4,
+          },
+        },
+      ],
+      aboutme: {
+        title: 'khoa',
+        description: 'khoa is a great',
+        image: this.imgSrc2,
+      },
+    },
+  ];
 }
--- src/app/pages/edit/user/edit.component.ts
+++ src/app/pages/edit/user/edit.component.ts
@@ -14,9 +14,27 @@ export class EditComponent {
   imgSrc2: string = 'assets/svg/arrow-narrow-right-svgrepo-com-black.svg';
   imgSrc3: string = 'assets/svg/profile-image-round-1326-svgrepo-com.svg';
   imgSrc4: string = 'assets/svg/pen-square-svgrepo-com.svg';
-  imgSrc5: string = '../../../../assets/svg/arrow-to-top-left-svgrepo-com.svg'
-  imgSrc6: string = '../../../../assets/svg/arrow-to-top-right-svgrepo-com.svg'
+  imgSrc5: string = '../../../../assets/svg/arrow-to-top-left-svgrepo-com.svg';
+  imgSrc6: string = '../../../../assets/svg/arrow-to-top-right-svgrepo-com.svg';
   bgClass: string = 'bg-gradient-to-r from-slate-900 to-slate-700';
   inputValues: Array<string> = ['username', 'email'];
-  inputValues2: Array<string> = ['old', '', 'confirm'];
+  inputValues2: Array<string> = ['old password', 'password', 'confirm password'];
+  currentIndex: number = 0;
+
+  updateCurrentIndex(index: number) {
+    this.currentIndex = index;
+  }
+
+  moveSlider(direction: number) {
+    const numberOfInputs = this.inputValues2.length;
+    this.currentIndex =
+      (this.currentIndex + direction + numberOfInputs) % numberOfInputs;
+    const translateXValue = `translateX(-${
+      (100 / numberOfInputs) * this.currentIndex
+    }%)`;
+    const slider = document.getElementById('slider');
+    if (slider) {
+      slider.style.transform = translateXValue;
+    }
+  }
 }
