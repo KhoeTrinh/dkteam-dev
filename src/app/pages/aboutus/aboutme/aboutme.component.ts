ba92e8f1bacf13b711dca731e3f2c002f96b7dd9
KhoeTrinh <kelvintrinh09@gmail.com>
2024-11-02 09:48:58 +0700

    pem pme done

--- src/app/pages/aboutus/aboutme/aboutme.component.ts
+++ src/app/pages/aboutus/aboutme/aboutme.component.ts
@@ -1,12 +1,22 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-aboutme',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './aboutme.component.html',
   styleUrl: './aboutme.component.css'
 })
 export class AboutmessComponent {
-
+  aboutmeData = {
+    id: '0c548a0a-4914-42f8-a5d0-887ba5f70f',
+    username: 'khoa',
+    userImage: 'assets/svg/user-svgrepo-com.svg',
+    aboutme: {
+      title: 'Khoa The Great Developer',
+      description: 'Khoa is a great developer',
+      image: 'assets/png/OIP.jpg'
+    }
+  }
 }
